ac7453aad31c7356f2eb6f156588dbd620f22f03
sibiu <144681144+razvantrufin99xx@users.noreply.github.com>
2024-06-18 08:52:48 +0300

    Add files via upload
    how to do it ?

---
+++ MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/17/2024
+ * Time: 8:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace codeTranslator
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(254, 369);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(347, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(70, 50);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(690, 20);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "20 INPUT  \"Enter Your Name\" , X ";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(70, 98);
+			this.textBox2.Multiline = true;
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(690, 246);
+			this.textBox2.TabIndex = 2;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(912, 457);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "codeTranslator";
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,142 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: razvan
+ * Date: 6/17/2024
+ * Time: 8:22 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace codeTranslator
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		
+		public string line = " ";
+		public string [] keywords = {" ","FOR","TO","NEXT","THEN","WHILE","IF","ELSE","ENDIF","DO","GOTO","SUB","END","PRINT","INPUT","PROCEDURE","FUNCTION","DIM","REDIM","INTEGER","REAL","DOUBLE","CHAR","STRING"};
+		public char[] separator = {' ','(',')','='};
+		public string [] linecode;// = line.Split(separator);
+		public int [] indexkeyqords = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+		
+		public void getLineOfCode(string s)
+		{
+			line  = s ;
+		}
+		
+		public int[] searchForKeyWords(int G)
+		{
+		
+			int i = 0;
+		
+			linecode = line.Split(separator);
+			int countergasite = -1;
+			while(i<linecode.Length-1)
+			{
+				for(int k = 0 ; k < keywords.Length-1;k++){
+					if(G==0){
+						if(linecode[i].ToString().ToUpper()!=keywords[k])
+						{
+							countergasite++;
+							try{
+								indexkeyqords[countergasite]=k;
+							}
+							catch{};
+							
+						}
+					}
+					else if(G==1)
+					{
+						if(linecode[i].ToString().ToUpper()==keywords[k])
+						{
+							countergasite++;
+							try{
+								indexkeyqords[countergasite]=k;
+							}
+							catch{};
+							
+						}
+					}
+				
+				
+					
+				}
+				i++;
+			};
+			return indexkeyqords;
+		}
+		
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			int typeofrezult = 1;
+			getLineOfCode(this.textBox1.Text);
+	
+			int [] rez = searchForKeyWords(0);
+			int i = 0;
+			string s = " ";
+			
+			//search founded 
+			if(typeofrezult==0){
+			while(i<rez.Length){
+			
+				s += rez[i].ToString() + " \r\n";
+			
+			
+				i++;
+			};
+			}
+			if(typeofrezult==2){
+			while(i<rez.Length){
+			
+					
+				s += keywords[rez[i]].ToString() +  " \r\n";
+				i++;
+			};
+			}
+			if(typeofrezult==1){
+			//show line of code atoms
+			 i = 0;
+			 string c = "";
+			 int FLAGOFOP =-1;
+			while(i<linecode.Length){
+			
+			 	c = linecode[i].ToString();
+			 	//if(c==" "){c="//*<SPACE>*//";}
+			 	//if(c==""){c="//*<EMPTY>*//";}
+			 	//if(c.Contains("2")==true){c="//*" + c + "*//"; }
+			 	if(c.Contains("2")==true){c=" ";}
+			 	if(c=="INPUT"){c="INPUT(";FLAGOFOP=1;}
+			 	//s += c + " : " + i.ToString() + " \r\n";
+			 	s+=c;
+				i++;
+			};
+			 if(FLAGOFOP==1){s+=");";}
+			}
+			this.textBox2.Text =  s;
+		}
+		
+	}
+}
